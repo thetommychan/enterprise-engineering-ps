35064a5c5f3c85d0a0d264e1775c5f93696aa36b
thetommychan <96588455+thetommychan@users.noreply.github.com>
2023-12-04 16:52:42 -0500

    Update del-profile-form-v8.ps1
    Removing CIDD

--- citrix/del-profile-form-v8.ps1
+++ citrix/del-profile-form-v8.ps1
@@ -22,7 +22,7 @@ $richTextBox1.SelectionStart = $richTextBox1.TextLength
 $richTextBox1.ScrollToCaret()
 
 
-#$outfile1 = "\\opnasi02\server\powershell\Shane\$username-delprof-$(get-date -f yyyMMdd-HHmmss).log"
+#$outfile1 = "\\share02\server\powershell\Tom\$username-delprof-$(get-date -f yyyMMdd-HHmmss).log"
 $newTextLine  | out-file -FilePath $outfile1 -Append 
 #Add-Content -Path $outfile1 -Value ($newTextLine)
 
@@ -74,14 +74,14 @@ $MainForum.Text = "Delete Profile"
 function stufftodo {
 $username=$TextBox.Text
 $richTextBox1.Clear()
- #$outfile1="c:\upgf\$username-delprof-$(get-date -f yyyMMdd-HHmmss).log" 
- $outfile1 = "\\opnasi02\helpdesk\delproflog\$username-delprof-$(get-date -f yyyMMdd-HHmmss).log"
+ #$outfile1="c:\domain\$username-delprof-$(get-date -f yyyMMdd-HHmmss).log" 
+ $outfile1 = "\\share02\helpdesk\delproflog\$username-delprof-$(get-date -f yyyMMdd-HHmmss).log"
  #out-file -Filepath $outfile1 -inputObject (get-date)
   OutputText "`r         $($runtimeuser.ToUpper())        is deleting profile for    $($username.ToUpper())" 
   OutputText "`r `r"
   OutputText "`r         Start Time:  $(get-date)" 
     OutputText "`r `r"
-  Del-UPSProfile($username)
+  Del-CompanyProfile($username)
   }
 
 
@@ -178,7 +178,7 @@ $MainForum.ShowDialog()| Out-Null
 #region "functions related to actual delete profile"
 
 
-function Del-UPSProfile
+function Del-CompanyProfile
 
 {
 <#
@@ -187,17 +187,17 @@ function Del-UPSProfile
 .DESCRIPTION
     Script deltes Citrix Profile. renames AppData to AppData.old, Creates new folder Appdata\Microsoft\Outlook and copies .nk2 file there.
     It goes through all XenApp servers in the new farm and looks for profile folder and if it finds any deletes it.
-    It uses Active Directory user's name in the form ABCDEFG without prefix (richmond\) or without suffix (@upgf.com)    
+    It uses Active Directory user's name in the form ABCDEFG without prefix (DomainName\) or without suffix (@domain.com)    
     It requires:
-    1.NTFSSecurity module (avoid the latest version of it from the internet. Use one from \\opnasi02\Server\scripts\DeleteAppData_PS)
+    1.NTFSSecurity module (avoid the latest version of it from the internet. Use one from \\share02\Server\scripts\DeleteAppData_PS)
     2.XenApp snapin (or implicit remoting session to one of the collectors)
 
     Note : It will stop if it finds any connection in the new farm.
 
 .EXAMPLE
-   Delete-UPSProfile -name APC1354
+   Delete-CompanyProfile -name APC1354
 .EXAMPLE
-    Delete-UPSProfile ABC1DEF
+    Delete-CompanyProfile ABC1DEF
 #>
 [CmdletBinding()]
 
@@ -224,7 +224,7 @@ $Exitbtn.Enabled=$false
     }
     catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException]
     {
-        OutputText "User $name does not exist. Check spelling?`rYou have to use TForce Freight user name NOT US\ name`r"
+        OutputText "User $name does not exist. Check spelling?`rYou have to use Company user name NOT US\ name`r"
         $allisgood = $false
     }
 
@@ -233,15 +233,15 @@ $Exitbtn.Enabled=$false
    {
         $who2 = $who.displayname
         OutputText "STEP 1: `rProcessing user $who2.`rChecking for existing sessions in XA7  `r"    
-        $connecttest13=test-connection wpctxi13 -quiet
-        $connecttest14=test-connection wpctxi14 -quiet
+        $connecttest13=test-connection CitrixController13 -quiet
+        $connecttest14=test-connection CitrixController14 -quiet
         If ($connecttest13)
         {
-            $controller="wpctxi13"
+            $controller="CitrixController13"
         }
             elseif ($connecttest14)
         {
-            $controller="wpctxi14"
+            $controller="CitrixController14"
         }
             else 
         {
@@ -251,7 +251,7 @@ $Exitbtn.Enabled=$false
         }
     }
     $nameupper = $name.ToUpper()
-    $sesscount = get-brokersession -adminaddress $controller -brokeringusername RICHMOND\$nameupper
+    $sesscount = get-brokersession -adminaddress $controller -brokeringusername DomainName\$nameupper
     $prop1a = $sesscount | Select-Object -ExpandProperty dnsname -Unique
     if ($sesscount.count -gt 0)
     {
@@ -268,7 +268,7 @@ $Exitbtn.Enabled=$false
     # WTC - 10/12/23 creating simple functions to help diagnose issues easier
     function Get-Ownership
     {
-        $testOwner = Get-UPSOwner $name
+        $testOwner = Get-CompanyOwner $name
         If ($testOwner.Owner -eq $null)
         {
             Take-Ownership $name
@@ -290,7 +290,7 @@ $Exitbtn.Enabled=$false
     # WTC - 10/12/23 creating simple functions to help diagnose issues easier
     function Get-EdgeBackup
     {
-        #2021-09-28 -- (WSH) -- Added section to make a backup of the MS Edge Bookmarks and Preferneces files       #  $redir="\\upgf.com\dfs-upgf\users\ntst002\redir"
+        #2021-09-28 -- (WSH) -- Added section to make a backup of the MS Edge Bookmarks and Preferneces files       #  $redir="\\domain.com\FileShare-domain\users\ntst002\redir"
         If(!(Test-Path "$redir\MSEdgeBookmarks_backup"))
         {
             If (Test-path "$redir\AppData\Microsoft\Edge\User Data\Default\Bookmarks")
@@ -304,7 +304,7 @@ $Exitbtn.Enabled=$false
     function Set-AppDataOld
     {
         Start-Job -Name "remAppDat" -ScriptBlock{
-        $redir = "\\upgf.com\DFS-UPGF\ctx_users\$using:Name\REDIR" 
+        $redir = "\\domain.com\FileShare-domain\ctx_users\$using:Name\REDIR" 
         $loop = Get-ChildItem $redir
         $newName = 'appdata.old'+((Get-date -f yyyyMMdd-HHmmss).tostring())
         foreach($folder in $loop)
@@ -388,11 +388,11 @@ $Exitbtn.Enabled=$false
         {
             [boolean]$AppdataIsGood = $true
             Outputtext "STEP 3: Deleting Appdata folder `r"
-            $redir = "\\upgf.com\DFS-UPGF\ctx_users\$Name\REDIR"
+            $redir = "\\domain.com\FileShare-domain\ctx_users\$Name\REDIR"
             $checkforappdata = Test-Path "$redir\appdata"
             if (!($checkforappdata))
             {
-                OutputText "Message: AppData folder at \\upgf.com\DFS-UPGF\users\$Name\REDIR does not exist.`rProceeding... `r`r"
+                OutputText "Message: AppData folder at \\domain.com\FileShare-domain\users\$Name\REDIR does not exist.`rProceeding... `r`r"
                 #Remove-AppDataOld; Handled in Set-AppDataOld function: WTC - 10/12/23 creating simple functions to help diagnose issues easier
                 Get-EdgeBackup;
                 Get-PreferencesBackup;
@@ -432,26 +432,26 @@ $Exitbtn.Enabled=$false
     If ($AllIsGood)
     {
         OutputText "STEP 2: Deleting Citrix XA7 Profile `r"
-        $fldrnames3b=Get-Childitem "\\upgf.com\dfs-upgf\xa7x_profiles\" -filter $name* -Directory    #   $fldr3b = "nfon157.v2"
+        $fldrnames3b=Get-Childitem "\\domain.com\FileShare-domain\xa7x_profiles\" -filter $name* -Directory    #   $fldr3b = "nfon157.v2"
         Foreach ($fldr3b in $fldrnames3b)
         {
-            $b=Test-Path "\\upgf.com\dfs-upgf\xa7x_profiles\$fldr3b"  
+            $b=Test-Path "\\domain.com\FileShare-domain\xa7x_profiles\$fldr3b"  
             if ($b=$true)
             {         
-                Remove-Item "\\upgf.com\dfs-upgf\xa7x_profiles\$fldr3b" -Recurse -Force  -ErrorAction SilentlyContinue  
-                $b1=Test-Path "\\upgf.com\dfs-upgf\xa7x_profiles\$fldr3b"
+                Remove-Item "\\domain.com\FileShare-domain\xa7x_profiles\$fldr3b" -Recurse -Force  -ErrorAction SilentlyContinue  
+                $b1=Test-Path "\\domain.com\FileShare-domain\xa7x_profiles\$fldr3b"
                 If ($b1=$true)
                 {
-                    OutputText "Citrix Profile \\upgf.com\dfs-upgf\xa7x_profiles\$fldr3b deleted `r"
+                    OutputText "Citrix Profile \\domain.com\FileShare-domain\xa7x_profiles\$fldr3b deleted `r"
                 }
                     else
                 {
-                    OutputText "Citrix Profile \\upgf.com\dfs-upgf\xa7x_profiles\$fldr3b was NOT deleted.  `r"
+                    OutputText "Citrix Profile \\domain.com\FileShare-domain\xa7x_profiles\$fldr3b was NOT deleted.  `r"
                 }
             }
                 else
             {
-                OutputText "WARNING: Citrix XA7 Profile at \\upgf.com\dfs-upgf\xa7x_profiles\$fldr3b does not exist.`rThat may not necessarily be a problem.`r"
+                OutputText "WARNING: Citrix XA7 Profile at \\domain.com\FileShare-domain\xa7x_profiles\$fldr3b does not exist.`rThat may not necessarily be a problem.`r"
             }
         }  
     } Remove-AppData # WTC - 10/12/23 creating simple functions to help diagnose issues easier
